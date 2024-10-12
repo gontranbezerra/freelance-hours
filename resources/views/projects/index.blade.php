b1cd1b79bac3997683b0af4f754f631078419810
Gontran Bezerra <gontranbezerra@gmail.com>
2024-10-12 18:53:57 -0300

    feat: aula 02: routes

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app>
+
+
+    <a href="{{ route('projects.show', 1) }}">
+        primeiro projeto
+    </a>
+
+</x-layouts.app>
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,10 @@
+{{-- <div> --}}
+{{-- <pre> --}}
+{{-- @dd(request()->route('project')) --}}
+{{-- @dd(request()->route()) --}}
+{{-- </pre> --}}
+{{-- </div> --}}
+
+<x-layouts.app>
+    show
+</x-layouts.app>
